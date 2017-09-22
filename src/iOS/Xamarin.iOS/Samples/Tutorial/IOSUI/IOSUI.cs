f403f12a5549d710dc8f33c48bcbf58983a7851f
Nathan Castle <ncastle@esri.com>
2017-09-22 15:16:33 -0700

    moving forward with UI prototyping

---
+++ src/iOS/Xamarin.iOS/Samples/Tutorial/IOSUI/IOSUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Esri.ArcGISRuntime.Mapping;
+using UIKit;
+
+namespace ArcGISRuntimeXamarin.Samples.IOSUI
+{
+    public partial class IOSUI : UIViewController
+    {
+        public IOSUI() : base("IOSUI", null)
+        {
+            myMapView.Map = new Map(Basemap.CreateImagery());
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
